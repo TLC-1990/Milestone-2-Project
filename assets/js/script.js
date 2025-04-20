4df2e22fb4c872effe6b87ce52dfde0bf3a2934d
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-04-20 13:03:39 +0000

    Add pre-game start panel. Add boardgame board below game cards

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function startPageMessage() {
+    var x = document.getElementById("gameboard");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
