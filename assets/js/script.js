c6ba5e2a62fa197339485ed1f9cdc4aa88e2f53f
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-04-29 21:10:44 +0000

    Add intial Let statements for game initiation to script.js. Resolve margin issue between cards. Set front card visibility to hidden through transform function in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 const introSectionContainer = document.getElementById("intro-section")
 console.log(introSectionContainer)
-const gameSectionContainer = document.getElementById("game-section-container")
-console.log(gameSectionContainer)
+
 
 const startScreenBtn = document.getElementById("start-screen-button")
 console.log(startScreenBtn)
+
+
+const gameSectionContainer = document.querySelector(".game-section-container")
+console.log(gameSectionContainer)
+let cards = []
+
+let firstCard, secondCard;
+let lockboard
