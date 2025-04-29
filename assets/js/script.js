d40a918d1ec6596d63cb0566d5e5875b95b31c65
TLC-1990 <lt5sksmq@students.codeinstitute.net>
2025-04-29 19:46:59 +0000

    Fix width 100% issue carusing mobile view bug

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const introSectionContainer = document.getElementById("intro-section")
+console.log(introSectionContainer)
+const gameSectionContainer = document.getElementById("game-section-container")
+console.log(gameSectionContainer)
+
+const startScreenBtn = document.getElementById("start-screen-button")
+console.log(startScreenBtn)
